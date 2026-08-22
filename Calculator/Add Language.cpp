171940f877bae2dcc49eadd0080ce11bc7daa2e7
ICodexaI <Edyard_Rey@protonmail.com>
2026-08-22 22:06:57 +0900

    Добавьте файлы проекта.

---
+++ Calculator/Add Language.cpp
@@ -0,0 +1,7 @@
+﻿#include "Add Language.h"
+
+void Russ()
+{
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
+}
---
+++ Calculator/All In.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <cmath>
+#pragma once
+#include <Windows.h>
+#include "Add Language.h"
